98638e67d6f7f898f404492400c5a66262f139a8
Nicolas <Nicolas_pereira27@hotmail.com>
2021-05-03 00:33:59 +0200

    First level OK

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,6 @@
 package sample;
 
-import java.io.File;
+import java.io.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,20 +12,25 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+    final int SCREEN_RATIO = 3;
+    int x = 0;
+    int y = 0;
 
     public static void main (String args []) {
         Application.launch(args);
     }
 
     public void start(Stage stage) throws Exception {
-        //Load files
-        File level1 = new File("./resources/level1");
-
         Pane pane = new Pane();
+        loadLevel("level1", pane);
+
         Scene scene = new Scene(pane);
 
 //        keyboard(pane, doodle, scene);
@@ -44,7 +49,6 @@ public class Main extends Application{
             public void run() {
                 Platform.runLater(new Runnable() {
                     public void run() {
-                        System.out.println("Running");
 //                        play(platformFile, pane, doodle);
                     }
                 });
@@ -52,6 +56,40 @@ public class Main extends Application{
         };
         fall.schedule(task, 0, 4);
 
+    }
+
+    private void loadLevel(String name, Pane pane) throws Exception {
+        pane.setBackground(new Background(new BackgroundFill(Color.web("322B28"), null, null)));
+
+        File level1 = new File("./resources/"+name);
+        BufferedReader br = new BufferedReader( new FileReader(level1));
+        String line;
+        while ((line = br.readLine()) != null){
+            String [] textures = line.split(";");
+            for(String texture : textures){
+                int id = Integer.parseInt(texture.split("x")[0]);
+                int nbr = Integer.parseInt(texture.split("x")[1]);
+                for(int i=0; i<nbr; i++){
+                    if(id!=-1) {
+                        Texture t = Texture.values()[id];
+                        pane.getChildren().add(loadImage(t));
+                        x += t.getX() * SCREEN_RATIO;
+                    }else{
+                        x += 8 * SCREEN_RATIO;
+                    }
+                }
+            }
+            x=0;
+            y+=8*SCREEN_RATIO;
+        }
+    }
+
+    private ImageView loadImage(Texture texture) throws Exception{
+        Image image = new Image( new FileInputStream("./resources/assets/"+texture.getImage()), texture.getX()*SCREEN_RATIO, texture.getY()*SCREEN_RATIO,false,false);
+        ImageView im = new ImageView(image);
+        im.setX(x);
+        im.setY(y);
+        return im;
     }
 
 //    private void keyboard(Pane pane, Object doodle, Scene scene) {
---
+++ src/sample/Texture.java
@@ -0,0 +1,33 @@
+package sample;
+
+public enum Texture {
+  backWallBlackHalf ("backWallBlackHalf.png", 16, 8), box ("box.png", 16, 16), coin ("coin.png",16,16), doorOpen ("doorOpen.png",32,32),
+  horiozontalBeam ("horiozontalBeam.png", 48, 16), rope ("rope.png",16,48), wall ("wall.png",16,16), backWallBlack ("backWallBlack.png",16,8),
+  cabinet ("cabinet.png",16,32), dawer ("dawer.png", 16,16), door ("door.png",16,24), key ("key.png", 16, 16), shelf ("shelf.png", 46, 16),
+  backWallHalf ("backWallHalf.png", 16, 8), chair1 ("chair1.png", 16,16), dirt ("dirt.png", 16,16), flag ("flag.png", 16, 48), ladderBot ("ladderBot.png", 16, 16),
+  stool ("stool.png", 16,16), backWall ("backWall.png",16,8), chair2 ("chair2.png", 16,16), door2Open ("door2Open.png", 32, 32), floor ("floor.png", 16, 16),
+  ladderTop ("ladderTop.png", 16,16), table ("table.png",32, 16), barrel ("barrel.png",16,16), chest ("chest.png", 32, 16), door2 ("door2.png",32,32),
+  flowers ("flowers.png", 16, 16), platform ("platform.png",16,8), verticalBeam ("verticalBeam.png", 16, 48), rightCorner("rightCorner.png", 16,16), leftCorner("leftCorner.png", 16,16);
+
+  private String image;
+  private int x;
+  private int y;
+
+  Texture (String image, int x, int y){
+      this.image = image;
+      this.x = x;
+      this.y = y;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+}
